f78058ad0956225de1eb65cbdc4252f112c665cd
Niko Nikolov <nnikolov@gocardless.com>
2022-11-10 16:08:58 +0000

    Set up some LSP keymaps

--- neovim/.config/nvim/lua/niko/lsp.lua
+++ neovim/.config/nvim/lua/niko/lsp.lua
@@ -1,3 +1,22 @@
-require'lspconfig'.gopls.setup{}
-require'lspconfig'.bashls.setup{}
-require'lspconfig'.pyright.setup{}
+local on_attach = function(client, bufnr)
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+ local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>dj', vim.diagnostic.goto_next, bufopts)
+  vim.keymap.set('n', '<leader>dk', vim.diagnostic.goto_prev, bufopts)
+end
+
+require'lspconfig'.gopls.setup{
+	on_attach = on_attach,
+}
+require'lspconfig'.bashls.setup{
+	on_attach = on_attach,
+}
+require'lspconfig'.pyright.setup{
+	on_attach = on_attach,
+}
